f6f1fd3d344360b12832dc84306869a874bbd9f0
yangjinda <yangjinda@youzan.com>
2020-05-19 10:27:07 +0800

    feat(*): 增加模块模式&修改readme

---
+++ topic/design-patterns/module-pattern/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const modulePattern = function () {
+    const name = "模块模式";
+    function privateFunction() {
+        console.log("私有函数", name);
+    }
+    function publicFunction() {
+        console.log("共有函数", name);
+    }
+    function publicToPrivateFunction() {
+        console.log("共有模式调用私有模式", name);
+        privateFunction();
+    }
+    return {
+        name: name,
+        publicFunction,
+        publicToPrivateFunction,
+    };
+};
+const moduleExample = modulePattern();
+console.log(moduleExample.name);
+moduleExample.publicFunction();
+moduleExample.publicToPrivateFunction();
