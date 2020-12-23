5005c1b8b59be92fb2347b909381ff292c1eadd4
yangjinda <yangjinda@youzan.com>
2020-12-23 15:18:28 +0800

    feat(test): test
    test

---
+++ daily/2020/2020-11/2020-11-04/jontyy/index.js
@@ -0,0 +1,63 @@
+const obj = {
+  kdtId: 18938611,
+  belongType: 0,
+  extra: {
+    alias: '1bpv3y9i9',
+    amountAtList: [
+      {
+        amountAt: 1,
+        enableDup: true,
+        groupIndex: 0,
+      },
+      {
+        amountAt: 1,
+        enableDup: true,
+        groupIndex: 1,
+      },
+    ],
+    desc: '2组商品任选2件',
+    discountType: 1,
+    discountValue: 400,
+    preferentialOverlay: 0,
+  },
+  id: 4303724169,
+  joinShopIds: [],
+  title: '测试组合套餐1215',
+  startAt: 1607935586000,
+  rangeShopType: 3,
+  goodsVOList: [
+    {
+      goodsId: 869382839,
+      skuId: 37102123,
+      groupIndex: 0,
+      extra: {
+        groupIndex: 0,
+      },
+    },
+    {
+      goodsId: 872902383,
+      skuId: 37072928,
+      groupIndex: 1,
+      extra: {
+        groupIndex: 1,
+      },
+    },
+    {
+      goodsId: 872902383,
+      skuId: 37072922,
+      groupIndex: 1,
+      extra: {
+        groupIndex: 1,
+      },
+    },
+    {
+      goodsId: 872902383,
+      skuId: 37072924,
+      groupIndex: 1,
+      extra: {
+        groupIndex: 1,
+      },
+    },
+  ],
+  activityStatus: 1,
+};
---
+++ leet-code/code-4/jontyy/index1.js
@@ -0,0 +1,89 @@
+const obj = {
+  refId: '996e17c1ede9dafd1f7e808d084b3328',
+  activityType: 208,
+  activityId: 4303724169,
+  title: '测试组合套餐1215',
+  startAt: 1607935586000,
+  delete: false,
+  superposedActivityTypes: [107, 106, 108, 257, 10, 100100],
+  offlineRangeType: 'part',
+  updatedAt: 1608088255000,
+  extra: {
+    alias: '1bpv3y9i9',
+    amountAtList: [
+      {
+        amountAt: 1,
+        enableDup: true,
+        groupIndex: 0,
+      },
+      {
+        amountAt: 1,
+        enableDup: true,
+        groupIndex: 1,
+      },
+    ],
+    desc: '2组商品任选2件',
+    discountType: 1,
+    discountValue: 400,
+    preferentialOverlay: 0,
+  },
+  activityStatus: 1,
+  goodsVOList: [
+    [
+      {
+        id: 984,
+        activityRefId: '996e17c1ede9dafd1f7e808d084b3328',
+        activityType: 208,
+        activityId: 4303724169,
+        groupId: null,
+        goodsId: 869382839,
+        skuId: 37102123,
+        extra: {
+          groupIndex: 0,
+        },
+      },
+    ],
+    [
+      {
+        id: 985,
+        activityRefId: '0bf16ed1ab9d38831ee10ed4ed1031ca',
+        activityType: 208,
+        activityId: 4303724169,
+        groupId: null,
+        goodsId: 872902383,
+        skuId: 37072928,
+        extra: {
+          groupIndex: 1,
+        },
+      },
+    ],
+    [
+      {
+        id: 986,
+        activityRefId: '472a35f87bfd5e6c4c5b9b939eae56b0',
+        activityType: 208,
+        activityId: 4303724169,
+        groupId: null,
+        goodsId: 872902383,
+        skuId: 37072922,
+        extra: {
+          groupIndex: 1,
+        },
+      },
+    ],
+    [
+      {
+        id: 987,
+        activityRefId: 'aec0c870b734d84eeaa7f954c70f6310',
+        activityType: 208,
+        activityId: 4303724169,
+        groupId: null,
+        goodsId: 872902383,
+        skuId: 37072924,
+        extra: {
+          groupIndex: 1,
+        },
+      },
+    ],
+  ],
+};
